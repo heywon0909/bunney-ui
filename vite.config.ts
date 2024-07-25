b01f600607f2952d9b5040eba2da3d8f5a496604
@hyewon_park2 <heywon_park2@tmax.co.kr>
2024-07-25 18:01:10 +0900

    feat: vite config setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: 'bunney',
+        },
+    },
+    plugins: [react(), svgr()],
 })
