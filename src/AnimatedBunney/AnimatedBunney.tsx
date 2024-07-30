7c362cd4141a3d1aa474fc77afb794650231d8b3
@hyewon_park2 <heywon_park2@tmax.co.kr>
2024-07-30 17:53:44 +0900

    feat: gsap library add

--- src/AnimatedBunney/AnimatedBunney.tsx
+++ src/AnimatedBunney/AnimatedBunney.tsx
@@ -1,24 +1,55 @@
 import { WholeBunney } from '@/common/styled'
 import * as S from './styled'
+import { useRef } from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import StartIcon from '../../public/Start.svg?react'
+// import { Box } from '@/common/Box'
 export interface BunneyProps {
     color?: string
     earColor?: string
     eyeColor?: string
 }
+gsap.registerPlugin(useGSAP)
 export const AnimatedBunney = ({ color, earColor, eyeColor }: BunneyProps) => {
+    const container = useRef<HTMLDivElement>(null)
+
+    useGSAP(
+        () => {
+            // Target the two specific elements we have asigned the animate class
+            gsap.to("[data-animate='rotate-1']", {
+                rotation: 40,
+                repeat: -1,
+                repeatDelay: 1,
+                yoyo: true,
+            })
+            gsap.to("[data-animate='rotate-2']", {
+                rotation: -40,
+                repeat: -1,
+                repeatDelay: 1,
+                yoyo: true,
+            })
+        },
+        { scope: container }
+    ) //
+
     return (
         <WholeBunney>
-            <S.EarWrapper>
-                <S.Ear dir="left" earColor={earColor} />
-                <S.Ear dir="right" earColor={earColor} />
+            <S.EarWrapper ref={container}>
+                <S.Ear dir="left" earColor={earColor} data-animate="rotate-2" />
+                <S.Ear
+                    dir="right"
+                    earColor={earColor}
+                    data-animate="rotate-1"
+                />
             </S.EarWrapper>
             <S.FaceWrapper color={color}>
                 <S.EyeWrapper>
                     <S.Eye eyeColor={eyeColor}>
-                        <StartIcon />
+                        <StartIcon className="box" />
                     </S.Eye>
                     <S.Eye eyeColor={eyeColor}>
-                        <StartIcon />
+                        <StartIcon className="box" />
                     </S.Eye>
                 </S.EyeWrapper>
             </S.FaceWrapper>
